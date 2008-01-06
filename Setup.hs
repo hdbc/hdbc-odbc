f97834c4777bbbee75f72f497a1a00dbe8a8f379
bjorn <bjorn@bringert.net>
2008-01-06 17:12:35 +0100

    hdbc-odbc: Use Cabal configurations. The odbc/odbc32 choice is now handled with os(win32) (untested as I don't have Windows).

--- Setup.hs
+++ Setup.hs
@@ -1,20 +1,5 @@
+#!/usr/bin/env runhaskell
+
 import Distribution.Simple
-import qualified System.Info
-import Data.List
-import Distribution.Simple.Utils
-import Distribution.PackageDescription
-import System.Exit
 
-main = defaultMainWithHooks defaultUserHooks{preConf = conf, postConf = ok}
-       where ok _ _ _ _ = return ExitSuccess
-
-conf args flags =
-    do config <- if isWindows
-                 then do putStrLn "On Windows -- using odbc32"
-                         return (emptyBuildInfo {extraLibs = ["odbc32"]})
-                 else do putStrLn "Not on Windows -- using odbc"
-                         return (emptyBuildInfo {extraLibs = ["odbc"]})
-       writeHookedBuildInfo "HDBC-odbc.buildinfo" (Just config, [])
-       return (Just config, [])
-
-    where isWindows = isPrefixOf "mingw" System.Info.os
+main = defaultMain
