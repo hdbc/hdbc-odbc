70336901f529b7cfedbcbe6894d12edc11189c64
John Goerzen <jgoerzen@complete.org>
2009-01-30 14:03:55 -0600

    Updating the tests

--- testsrc/SpecificDB.hs
+++ testsrc/SpecificDB.hs
@@ -5,3 +5,18 @@ import Test.HUnit
 
 connectDB = 
     handleSqlError (connectODBC "DSN=hdbctest")
+
+
+-- These are copied from PostgreSQL for now, except for interval
+dateTimeTypeOfSqlValue :: SqlValue -> String
+dateTimeTypeOfSqlValue (SqlLocalDate _) = "date"
+dateTimeTypeOfSqlValue (SqlLocalTimeOfDay _) = "time without time zone"
+dateTimeTypeOfSqlValue (SqlZonedLocalTimeOfDay _ _) = "time with time zone"
+dateTimeTypeOfSqlValue (SqlLocalTime _) = "timestamp without time zone"
+dateTimeTypeOfSqlValue (SqlZonedTime _) = "timestamp with time zone"
+dateTimeTypeOfSqlValue (SqlUTCTime _) = "timestamp with time zone"
+dateTimeTypeOfSqlValue (SqlDiffTime _) = "integer"
+dateTimeTypeOfSqlValue (SqlPOSIXTime _) = "integer"
+dateTimeTypeOfSqlValue (SqlEpochTime _) = "integer"
+dateTimeTypeOfSqlValue (SqlTimeDiff _) = "integer"
+dateTimeTypeOfSqlValue _ = "text"
