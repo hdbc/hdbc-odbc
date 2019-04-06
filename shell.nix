86dc63165071d1bcbe4bb67a4dcbc709bf0c7bd2
Anton Desyatov <anton.dessiatov@gmail.com>
2019-04-06 17:33:44 +0700

    Added a couple of stack.yaml files for various Stackage snapshots
    Also added shell.nix so that it's easier to develop with NixOS

---
+++ shell.nix
@@ -0,0 +1,19 @@
+let hie = import(builtins.fetchGit {
+      name = "hie-nix";
+      url = "https://github.com/domenkozar/hie-nix.git";
+      ref = "master";
+      rev = "6794005f909600679d0b7894d0e7140985920775";
+    }) {};
+    nixpkgs = import(builtins.fetchGit {
+      name = "hixpkgs";
+      url = "https://github.com/NixOS/nixpkgs.git";
+      ref = "master";
+      rev = "2e501366033740c3ad1c5ca298d88add13a9f8a6";
+      }) {};
+in
+nixpkgs.stdenv.mkDerivation {
+  name = "hie-env";
+  buildInputs =
+    [ nixpkgs.stack nixpkgs.zlib.dev nixpkgs.zlib.out hie.hies
+    ];
+}
